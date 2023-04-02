93da639f59e0781098f1e15e48b5d560eee5e10e
ZviezdilinArt <118184844+ZviezdilinArt@users.noreply.github.com>
2023-04-02 14:47:45 +0300

    task#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const list = document.querySelector('#ingredients');
+
+const markup = ingredients.map(element => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = element;
+  return listItem;
+});
+
+list.append(...markup);
