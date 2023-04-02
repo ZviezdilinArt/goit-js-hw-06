b1c051a11a82659b78f71924f930741ae5cf3da2
ZviezdilinArt <118184844+ZviezdilinArt@users.noreply.github.com>
2023-04-02 15:38:03 +0300

    task#8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,11 @@
+const form = document.querySelector(".login-form");
 
-const ourForm = document.querySelector(".login-form");
+form.addEventListener("submit", sendForm);
 
-ourForm.addEventListener("submit", sendForm);
-
-function sendForm(event) {
-    event.preventDefault();
-    const email = event.currentTarget.elements.email.value;
-    const password = event.currentTarget.elements.password.value;
+function sendForm(evt) {
+    evt.preventDefault();
+    const email = evt.currentTarget.elements.email.value;
+    const password = evt.currentTarget.elements.password.value;
 
     if (email === "" || password === "") {
         return alert ("всі поля повинні бути заповнені");
@@ -19,5 +18,5 @@ function sendForm(event) {
 
     console.log(elements);
 
-    ourForm.reset();
+    form.reset();
 }
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const ourBtn = document.querySelector(".change-color");
+const colorNow = document.querySelector(".color");
+const ourBody = document.body;
+
+ourBtn.addEventListener("click", changeColor);
+
+function changeColor(evt) {
+  const changeColor = getRandomHexColor();  
+
+  ourBody.style.backgroundColor = changeColor;
+  colorNow.textContent = changeColor;
+}
